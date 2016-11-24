7c04a11c03a0787e36b871d028cbcf87902e777d
Artur Lane <arturlan.us@gmail.com>
2016-11-24 11:25:45 -0500

    initial commit

---
+++ weathernyc/weathernyc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weathernyc
+//
+//  Created by Artur Lan on 11/24/16.
+//  Copyright © 2016 Artur Lan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weathernyc/weathernyc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weathernyc
+//
+//  Created by Artur Lan on 11/24/16.
+//  Copyright © 2016 Artur Lan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
